b3f19687e84171548d6d14d4cf95117e7139b6a5
Teja Naraparaju <tnarapa1@jhu.edu>
2015-03-02 12:21:02 -0500

    removed unsed partials and mofied the angular service

--- js/app/services.js
+++ js/app/services.js
@@ -3,5 +3,12 @@ var app = angular.module('app');
 app.factory('scoreService', ['$http', function($http){
 	var nbaUrl = 'http://stats.nba.com/stats/scoreboardV2?DayOffset=0&LeagueID=00&gameDate=03%2F02%2F2015';
 
-	
+	return{
+		get: function(){
+			$http.get(
+
+				);
+		}
+	}
+
 }]);
